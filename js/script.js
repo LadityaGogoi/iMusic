76ad43f17076c7930ee5a1a42a360b7eb8b7b45f
Laditya_29 <108052802+dr-jackaal@users.noreply.github.com>
2022-12-01 17:24:57 +0530

    Update script.js
    adding initial javascript to the site

--- js/script.js
+++ js/script.js
@@ -1,7 +1,58 @@
 // script of spotify
 
+// Initialize the variables
+
+let masterPlay = document.getElementById('masterPlay');
+let gif = document.getElementById('gif');
+let myProgressBar = document.getElementById('myProgressBar');
+
+
 console.log("Welcome to beats");
 
-let audioElement = new Audio('songs/1.mp3');
+let songs = [
+    {songName: "NCS-1", filePath: "songs/1.mp3", coverPath: "image/covers/1.jpg"},
+    {songName: "NCS-2", filePath: "songs/2.mp3", coverPath: "image/covers/2.jpg"},
+    {songName: "NCS-3", filePath: "songs/3.mp3", coverPath: "image/covers/3.jpg"},
+    {songName: "NCS-4", filePath: "songs/4.mp3", coverPath: "image/covers/4.jpg"},
+    {songName: "NCS-5", filePath: "songs/5.mp3", coverPath: "image/covers/5.jpg"}
+]
 
-audioElement.play();
+let audioElement = new Audio('songs/4.mp3');
+
+
+// listen to event
+
+masterPlay.addEventListener('click', ()=>
+{
+
+    if(audioElement.paused || audioElement.currentTime <= 0)
+    {
+        audioElement.play();
+        masterPlay.classList.remove('fa-play');
+        masterPlay.classList.add('fa-pause');
+        gif.style.opacity = 1;
+    }
+    else
+    {
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause');
+        masterPlay.classList.add('fa-play');
+        gif.style.opacity = 0;
+    }
+
+
+});
+
+// audioElement.play();
+
+audioElement.addEventListener('timeupdate', ()=> {
+    let progress = parseInt((audioElement.currentTime/audioElement.duration) * 100);
+    myProgressBar.value = progress;
+
+});
+
+// seekbar changes
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
+})
